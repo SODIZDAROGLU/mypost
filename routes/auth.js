d8f2382a4bcf4ae5e8b4914d19f3514b8fd50ff5
sodizdaroglu <americanlaz@gmail.com>
2020-08-18 17:02:26 -0400

    making better

--- routes/auth.js
+++ routes/auth.js
@@ -12,6 +12,7 @@ const sendgridTransport = require('nodemailer-sendgrid-transport')
 const {SENDGRID_API,EMAIL} = require('../config/keys')
 
 
+
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth:{
         api_key:SENDGRID_API
